6b649a42de8de3f4e3bd5bfed2c7fd5328c39180
Reid Madock <reidmadock@gmail.com>
2023-02-28 11:23:28 -0600

    added readme with screenshot

--- script.js
+++ script.js
@@ -1,30 +1,14 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-renderLastMessage();
-
-var rowMessage = {
-  rowId: "",
-  rowTxt: ""
-};
-
-function renderLastMessage() {
-  localStorage.getItem("saved-task")
-}
-
 $(function () {
   var allRows = $(".container-lg").find(".time-block");
-  var allButtons = $(".container-lg").find(".saveBtn");
-  var allTextAreas = $(".container-lg").find(".description")
   var currentHour = dayjs().hour();
-  console.log(allButtons);
 
+  /* Iterate over every row element on page, check the time, check for local storage entry. */
   for (var i = 0; i < allRows.length; i++) {
     var rowIds = $(allRows[i]).prop('id');
-    console.log(allRows[i]);
     var blockTime = parseInt(rowIds.slice(5));
     var currentClass = $(allRows[i]).prop('class');
-    var savedData = localStorage.getItem("block-" + blockTime);
+    var savedData = localStorage.getItem(rowIds);
+
     if (blockTime < currentHour) {
       $(allRows[i]).removeClass(currentClass).addClass('row time-block past')
     }
@@ -34,23 +18,16 @@ $(function () {
     if (blockTime > currentHour) {
       $(allRows[i]).removeClass(currentClass).addClass('row time-block future')
     }
-    // localStorage.setItem(allRows[i],"");
     if (savedData) {
       $(allRows[i]).find("textarea").val(savedData);
     }
   }
-
+  /*  Grab the hour from the parent, grab the description from the clicked block, store with ID*/
   $('.saveBtn').on('click', function () {
-    // console.log("HEATHER WAS RIGHT");
-    // console.log($(this).find(".description"));
-    // console.log(this.parentElement);
-    // var currText = this.parentElement.childNodes[3].value
-    // localStorage.setItem("saved-task",currText);
     var timeBlock = $(this).parent().prop('id');
     var blockText = $(this).siblings('.description').val();
-    localStorage.setItem("block-" + timeBlock.slice(5), blockText);
+    localStorage.setItem(timeBlock, blockText);
   })
-  // TODO: Add code to display the current date in the header of the page.
 });
 
 $(function () {
